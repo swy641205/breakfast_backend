e6883dbc5ae7b3127d1841cffb5a39363fe0add7
swy <minicr1234@gmail.com>
2024-06-20 13:49:41 +0800

    refactor: Update menu API to use JWT_SECRET from config

--- src/routers/menu.ts
+++ src/routers/menu.ts
@@ -3,13 +3,12 @@ import type {
     Request,
     Response
 } from "express-serve-static-core";
+import { JWT_SECRET } from '../config';
+import verifyToken from "../routers/users";
+import tbl from "../mysql/general";
 
 export const router = Router();
 
-const secret = process.env.JWT_SECRET;
-import verifyToken from "../routers/users";
-
-import tbl from "../mysql/general";
 const tblMenu = tbl('menu');
 
 router.get("/:id", async (req: Request, res: Response) => {
@@ -20,11 +19,11 @@ router.get("/:id", async (req: Request, res: Response) => {
     }
     token = token.split(' ')[1];
     try {
-        const payload = await verifyToken(token, secret);
+        const payload = await verifyToken(token, JWT_SECRET);
     } catch (err) {
         return res.json(err);
     }
-    
+
     const id = req.params.id;
     const menu = await tblMenu.get('id', id);
     if (menu) {
@@ -42,11 +41,11 @@ router.get("/", async (req: Request, res: Response) => {
     }
     token = token.split(' ')[1];
     try {
-        const payload = await verifyToken(token, secret);
+        const payload = await verifyToken(token, JWT_SECRET);
     } catch (err) {
         return res.json(err);
     }
-    
+
     const id = req.params.id;
     const menuAll = await tblMenu.getAll();
     if (menuAll) {
@@ -63,7 +62,7 @@ router.post("/", async (req: Request, res: Response) => {
         return res.status(401).json({ message: "Please login first", code: 401 });
     }
     token = token.split(' ')[1];
-    const { roles } = await verifyToken(token, secret);
+    const { roles } = await verifyToken(token, JWT_SECRET);
     if (!roles.includes('admin' || 'owner')) {
         return res.status(401).json({ message: "permission deny", code: 403 });
     }
@@ -98,7 +97,7 @@ router.put("/:id", async (req: Request, res: Response) => {
         return res.status(401).json({ message: "Please login first", code: 401 });
     }
     token = token.split(' ')[1];
-    const { roles } = await verifyToken(token, secret);
+    const { roles } = await verifyToken(token, JWT_SECRET);
     if (!roles.includes('admin' || 'owner')) {
         return res.status(401).json({ message: "permission deny", code: 403 });
     }
@@ -134,7 +133,7 @@ router.delete("/:id", async (req: Request, res: Response) => {
         return res.status(401).json({ message: "Please login first", code: 401 });
     }
     token = token.split(' ')[1];
-    const { roles } = await verifyToken(token, secret);
+    const { roles } = await verifyToken(token, JWT_SECRET);
     if (!roles.includes('admin' || 'owner')) {
         return res.status(401).json({ message: "permission deny", code: 403 });
     }
