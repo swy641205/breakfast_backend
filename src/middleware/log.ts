b148c786c2a0ff21ea795d55c23f0a7a2cf9a9de
swy <minicr1234@gmail.com>
2024-06-20 11:52:17 +0800

    refactor: middleware log.ts

--- src/middleware/log.ts
+++ src/middleware/log.ts
@@ -1,4 +1,4 @@
-import { Request, Response, NextFunction } from 'express';
+import type { Request, Response, NextFunction } from 'express';
 
 const logger = (req: Request, res: Response, next: NextFunction) => {
     console.log(`${req.timestamp}: ${req.ip} ${req.method} ${req.originalUrl}`);
